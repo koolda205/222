0cb5ee29976846dade27f9d4ef8887281d04daeb
Dmitrii Kulakov <koolda@yandex.ru>
2023-05-23 16:40:18 +0300

    add in method public void configureViewResolvers (ViewResolverRegistry registry): resolver.setCharacterEncoding("UTF-8"); resolver.setContentType("text/html; charset=UTF-8"); and add in method метод templateResolver добавляем 1 строчку:                   templateResolver.setCharacterEncoding("UTF-8");

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -28,6 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/pages/");
+        templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setSuffix(".html");
         return templateResolver;
     }
@@ -45,6 +46,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setContentType("text/html; charset=UTF-8");
         registry.viewResolver(resolver);
     }
 }
