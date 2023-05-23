cb892554f5ac5ada18d3e675978af2d3958c4e1d
Dmitrii Kulakov <koolda@yandex.ru>
2023-05-23 16:53:47 +0300

    Add Car class Add CarService class and Interface

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,70 @@
+package model;
+
+
+import java.util.Objects;
+
+public class Car {
+
+
+    private Long id;
+
+
+    private String model;
+
+
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getSeries(int carSeries) {
+        return series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series && Objects.equals(id, car.id) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, series);
+    }
+}
